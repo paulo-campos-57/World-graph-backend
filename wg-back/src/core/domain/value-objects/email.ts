a666cb4492a2c5b407bc36351d7b3c305efde2ef
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2026-08-24 16:55:15 -0300

    [ADD] - create email ValueObject

---
+++ wg-back/src/core/domain/value-objects/email.ts
@@ -0,0 +1,30 @@
+export class Email {
+    private readonly value: string;
+
+    constructor(email: string) {
+        if (!this.validate(email))
+            throw new Error(`Formato inválido para e-mail`);
+
+        this.value = email;
+    }
+
+    /**
+     * Valida a estrutura de uma string de e-mail.
+     * 
+     * Remove espaços nas extremidades e converte o e-mail para letras minúsculas 
+     * antes de testar a expressão regular.
+     * 
+     * @param email - A string contendo o e-mail a ser validado.
+     * @returns `true` se o e-mail tiver um formato válido, caso contrário `false`.
+     */
+    private validate(email: string) {
+        const cleanEmail = email.trim().toLowerCase();
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        return emailRegex.test(cleanEmail);
+    }
+
+    public getValue(): string {
+        return this.value;
+    }
+}
--- wg-back/src/core/domain/value-objects/name.ts
+++ wg-back/src/core/domain/value-objects/name.ts
@@ -3,11 +3,19 @@ export class Name {
 
     constructor(name: string) {
         if (!this.validate(name))
-            throw new Error(`O nome deve ter entre 2e 50 carcteres`);
+            throw new Error(`O nome deve ter entre 2 e 50 carcteres`);
 
         this.value = name.toLowerCase().trim();
     }
 
+    /**
+     * Valida a estrutura de uma string de nome.
+     * 
+     * Remove espaços nas extremidades antes de verificar o comprimento do nome.
+     * 
+     * @param name - A string contendo o nome a ser validado.
+     * @returns `isValid` se o nome for válido, essa variável é retornada como true, caso contrário, false.
+     */
     private validate(name: string) {
         const trimmedName = name.trim();
         const isValid = trimmedName.length < 2 || trimmedName.length > 50;
