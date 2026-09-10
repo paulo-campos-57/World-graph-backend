97c188eaa38caf6e48e03bd0a937c02bf28058fc
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2026-09-10 19:38:57 -0300

    [ADD] - create user-repository.interface.ts

---
+++ wg-back/src/core/use-cases/ports/user-repository.interface.ts
@@ -0,0 +1,7 @@
+import { User } from '../../domain/entities/user';
+import { Email } from '../../domain/value-objects/email';
+
+export interface IUserRepository {
+  create(user: User): Promise<boolean>;
+  findByEmail(email: Email): Promise<User>;
+}
